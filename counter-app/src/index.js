d3cb757234022da0b1e4442975f586e4fb169d6c
Sadman Sobhan <imran110219@gmail.com>
2021-01-16 00:09:45 +0600

    frontend add

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Counters from "./components/counters";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
