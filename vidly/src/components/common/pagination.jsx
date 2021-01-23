125e5b53333ed964ff8c4a8fb18e2ec8c9b96a65
Sadman Sobhan <imran110219@gmail.com>
2021-01-23 23:25:35 +0600

    frontend add

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -24,7 +24,7 @@ const Pagination = (props) => {
     </nav>;
 }
 
-Pagination.PropTypes = {
+Pagination.propTypes = {
     itemsCount: PropTypes.number.isRequired, 
     pageSize: PropTypes.number.isRequired, 
     currentPage: PropTypes.number.isRequired, 
