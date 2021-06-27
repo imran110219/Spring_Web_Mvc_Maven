1f6a68dcd24a0a77b5689b46b1b79555894ef6b7
Sadman Sobhan <imran110219@gmail.com>
2021-06-27 10:15:47 +0600

    Maximum Minimum Sale chart implemented

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import "./App.css";
-import Customers from './components/customers';
-import NotFound from './components/notFound';
-import Rentals from './components/rentals';
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
-import MovieForm from './components/movieForm';
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
-      );
+    );
   }
 }
 
--- vidly/src/components/navBar.jsx
+++ vidly/src/components/navBar.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return ( 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
         Vidly
       </Link>
@@ -29,10 +29,16 @@ const NavBar = () => {
           <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
           </NavLink>
+          <NavLink className="nav-item nav-link" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
+          </NavLink>
         </div>
       </div>
     </nav>
-     );
-}
- 
+  );
+};
+
 export default NavBar;
